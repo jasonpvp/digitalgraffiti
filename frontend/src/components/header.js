ac05c3f02bc6008d5c5c77d30d9e30c2b94dd9e6
Melissa Smith <msmith@newrelic.com>
2020-02-22 11:21:56 -0800

    install rebass

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,31 +3,16 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+  <header>
+    <Link
+      to="/"
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        color: `black`,
+        textDecoration: `none`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+      {siteTitle}
+    </Link>
   </header>
 )
 
