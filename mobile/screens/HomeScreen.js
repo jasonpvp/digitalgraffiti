dae0f3bd230ca18acab08bc75148e62b165e7c5f
Jason Van Pelt <jvanpelt@newrelic.com>
2020-03-10 20:40:16 -0700

    support svg images

--- mobile/screens/HomeScreen.js
+++ mobile/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react
 import { ScrollView } from 'react-native-gesture-handler';
 import * as WebBrowser from 'expo-web-browser';
 import MessageContext from '../contexts/messageContext';
+import SvgLogo from '../assets/images/logo.svg'
 
 import { MonoText } from '../components/StyledText';
 
@@ -16,10 +17,7 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.welcomeContainer}>
-        <Image
-          source={require('../assets/images/logo.gif')}
-          style={styles.welcomeImage}
-        />
+        <SvgLogo width='200' height='200' />
       </View>
 
       <View style={styles.getStartedContainer}>
@@ -34,29 +32,6 @@ export default function HomeScreen() {
 HomeScreen.navigationOptions = {
   header: null,
 };
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
 
 function handleLearnMorePress() {
   WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
